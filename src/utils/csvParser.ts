7173b688031bec04cffba2b176139e9dd77be8c7
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2024-08-09 22:16:24 +0000

    commented csvparser as not used

--- src/utils/csvParser.ts
+++ src/utils/csvParser.ts
@@ -1,15 +1,15 @@
-import Papa from 'papaparse';
+// import Papa from 'papaparse';
 
-export function parseCSV(file: File): Promise<any[]> {
-  return new Promise((resolve, reject) => {
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        resolve(results.data);
-      },
-      error: (error) => {
-        reject(error);
-      },
-    });
-  });
-}
+// export function parseCSV(file: File): Promise<any[]> {
+//   return new Promise((resolve, reject) => {
+//     Papa.parse(file, {
+//       header: true,
+//       complete: (results) => {
+//         resolve(results.data);
+//       },
+//       error: (error) => {
+//         reject(error);
+//       },
+//     });
+//   });
+// }

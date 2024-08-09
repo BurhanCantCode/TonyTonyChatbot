4d318f2df62a87abdb8fa16f3a84c9e967ffc3da
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2024-08-09 21:55:05 +0000

    commented shi

--- chatbot-creator/src/reportWebVitals.ts
+++ chatbot-creator/src/reportWebVitals.ts
@@ -1,17 +1,17 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB, Metric } from 'web-vitals';
+// import { Metric } from 'web-vitals';
 
-type ReportHandler = (metric: Metric) => void;
+// type ReportHandler = (metric: Metric) => void;
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+// const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
 
-export default reportWebVitals;
+// export default reportWebVitals;
